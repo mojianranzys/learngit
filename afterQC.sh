9d5f15953373ffac2c09aa9ee701037ddb49b613
mojianranzys <zhaoys@haplox.com>
2018-07-20 09:16:54 +0800

    afterQC

---
+++ afterQC.sh
@@ -0,0 +1,4 @@
+ pypy /tools/after/after.py \
+ -1 /haplox/users/zhaoys/rawfq/S001_20180404003-3_cfdna_pan-cancer-v1_6266_S1_R1_001.fastq.gz
+ -2 /haplox/users/zhaoys/rawfq/S001_20180404003-3_cfdna_pan-cancer-v1_6266_S1_R2_001.fastq.gz
+ -g /haplox/users/zhaoys/test/learngit/AfterQC/test/good --no_overlap >log 2>&1
