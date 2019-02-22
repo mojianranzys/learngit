f9efa60431d32a6dc1635b041309f618e25bf2b8
zys <1255886933@qq.com>
2019-02-22 15:55:06 +0800

    merge data to csv

---
+++ merge_csv.R
@@ -0,0 +1,58 @@
+#!/usr/bin/Rscript
+library(stringr)
+
+args <- commandArgs(TRUE)
+input <- args[1]
+input2 <- args[2]
+sampleSheet <- paste0("/haplox/runPipelineInfo/",  input,  "/sequence_", input,  ".csv")
+sampleSheet2 <- paste0("/haplox/runPipelineInfo/",  input2,  "/sequence_", input2,  ".csv")
+
+    csv_df1 <- read.csv(sampleSheet, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="latin1", encoding="UTF-8")
+    csv_df2 <- read.csv(sampleSheet2, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="latin1", encoding="UTF-8")
+
+    NCOL <- ncol(csv_df1)
+    NCOL2 <- ncol(csv_df2)
+    csv_df1[, (NCOL+1):(NCOL+5)] <- str_match(csv_df1[,1], "S(.*?)_(.*?)_(.*?)_(.*?)_(\\d+)")[, 2:6]
+    csv_df2[, (NCOL2+1):(NCOL2+5)] <- str_match(csv_df2[,1], "S(.*?)_(.*?)_(.*?)_(.*?)_(\\d+)")[, 2:6]
+    oss_dir_input <- paste0("oss://sz-hapseq/rawfq/", "20", substr(input, 1, 4), "/", input,  "_clinic/")
+    oss_dir_input2 <- paste0("oss://sz-hapseq/rawfq/", "20", substr(input2, 1, 4), "/", input2,  "_clinic/")
+    cmd <- paste0("ossutil ls ", oss_dir_input)
+    cmd2 <- paste0("ossutil ls ", oss_dir_input2)
+ #   print(cmd)
+    txt <- system(cmd, intern = TRUE)
+    txt2 <- system(cmd2, intern = TRUE) 
+#    print(txt)
+    myStrsplit <- function(x, split_para){
+        unlist(strsplit(x, split=split_para))[8]
+    }
+    txt_input_R1 <- unlist(lapply(txt[grepl("oss://.*_clinic\\/S\\d+.*R1.*fastq.gz", txt)], myStrsplit, split_para="\\s+"))
+    txt_input_R2 <- unlist(lapply(txt[grepl("oss://.*_clinic\\/S\\d+.*R2.*fastq.gz", txt)], myStrsplit, split_para="\\s+"))
+    txt_input2_R1 <- unlist(lapply(txt2[grepl("oss://.*_clinic\\/S\\d+.*R1.*fastq.gz", txt2)], myStrsplit, split_para="\\s+"))
+    txt_input2_R2 <- unlist(lapply(txt2[grepl("oss://.*_clinic\\/S\\d+.*R2.*fastq.gz", txt2)], myStrsplit, split_para="\\s+"))
+#    print(txt_input)
+    myStrsplit <- function(x, split_para){
+        unlist(strsplit(x, split=split_para))[8]
+    }
+    out_csv <- paste0("/haplox/users/zhaoys/merge_clinic/merge_",input,"_",input2,".csv")
+    if(!file.exists(out_csv)){
+        file.create(out_csv, recursive = TRUE)
+        }else{print("this file is exits")
+        }
+    for(i in seq(nrow(csv_df1))){
+            for(j in seq(nrow(csv_df2))){
+                if(csv_df1[i, 3] == csv_df2[j, 3] && csv_df1[i, 2] == csv_df2[j, 2] && csv_df1[i, 9] == csv_df2[j, 9]){
+                    merge_input_R1 <- txt_input_R1[grepl(csv_df1[i,1], txt_input_R1)]
+                    merge_input_R2 <- txt_input_R2[grepl(csv_df1[i,1], txt_input_R2)]
+                    merge_input2_R1 <- txt_input2_R1[grepl(csv_df2[j,1], txt_input2_R1)]
+                    merge_input2_R2 <- txt_input2_R2[grepl(csv_df2[j,1], txt_input2_R2)]
+                    rawfq_R1_1 <- gsub("oss://sz-hapseq","",merge_input_R1)
+                    rawfq_R2_1 <- gsub("oss://sz-hapseq","",merge_input_R2)
+                    rawfq_R1_2 <- gsub("oss://sz-hapseq","",merge_input2_R1)
+                    rawfq_R2_2 <- gsub("oss://sz-hapseq","",merge_input2_R2)
+#                    print(rawfq_R1_1)
+                    sink(out_csv,append=TRUE)
+                    cat(paste0(rawfq_R1_1,",", rawfq_R1_2,",","1","\n", rawfq_R2_1,",", rawfq_R2_2,",","1","\n", step=""))
+                    sink()
+                }
+            }
+        }
