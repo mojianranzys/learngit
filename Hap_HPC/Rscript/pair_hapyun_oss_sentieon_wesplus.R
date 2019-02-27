f765909d18e5498368f7790cc76266b0cc053e9d
zys <1255886933@qq.com>
2019-02-27 14:12:32 +0800

    updata wesplus

--- Hap_HPC/Rscript/pair_hapyun_oss_sentieon_wesplus.R
+++ Hap_HPC/Rscript/pair_hapyun_oss_sentieon_wesplus.R
@@ -34,82 +34,53 @@ if(!file.exists(wk)){
                     dir.create(out, recursive = TRUE)
                 }
                 sink(sh_file)
-                out_nohup <- paste0(rawout,"/",tumor_df[i,1],"/",tumor_df[i,1],".nohup.out")
-                out_error <- paste0(rawout,"/",tumor_df[i,1],"/",tumor_df[i,1],".nohup.out")
-                cnv_in <-paste0(rawout,"/",tumor_df[i,1],"/cnv-pair_node_25")
-                cnv_out <- paste0(rawout,"/",tumor_df[i,1],"/cnv")
-                cnv_rename <- paste0('"s/0001.sample/',tumor_df[i,1],'/"')
-                qc_in <- paste0(rawout,"/",tumor_df[i,1],"/fastp-16core64g_*/reports/*")
-                qc_out <- paste0(rawout,"/",tumor_df[i,1],"/QC")
-                MutScan_in <- paste0(rawout,"/",tumor_df[i,1],"/MutScan_node_24")
-                MutScan_out <- paste0(rawout,"/",tumor_df[i,1],"/mutscan_",tumor_df[i,1])
-                MutScan_rename<-paste0('"s/0001.MutScan_out/',tumor_df[i,1],'_mutscan/"')
-                fusionscan_in <- paste0(rawout,"/",tumor_df[i,1],"/genefuse_huang_test_node_22")
-                fusionscan_out <- paste0(rawout,"/",tumor_df[i,1],"/fusionscan")
-                fusionscan_rn_1_in <- paste0(rawout,"/",tumor_df[i,1],"/fusionscan/0001.genefuse_huang_test_out_html_output_22.html")
-                fusionscan_rn_1_out <-  paste0(rawout,"/",tumor_df[i,1],"/fusionscan/",tumor_df[i,1],"_fusion.html")
-                fusionscan_rn_2_in <- paste0(rawout,"/",tumor_df[i,1],"/fusionscan/0001.genefuse_huang_test_out_json_output_22.json")
-                fusionscan_rn_2_out <-  paste0(rawout,"/",tumor_df[i,1],"/fusionscan/",tumor_df[i,1],"_fusion.json")
-                germline_mv_1 <- paste0(rawout,"/",tumor_df[i,1],"/germline-sentieon-16core64g_node_38")
-                germline_mv_out <- paste0(rawout,"/",tumor_df[i,1],"/hapyun")
-                germline_rn <- paste0('"s/germline/',tumor_df[i,"normal"],'/"')
-                germline_mv_2 <- paste0(rawout,"/",tumor_df[i,1],"/germline")
-                mrbam_in <- paste0(rawout,"/",tumor_df[i,1],"/MrBam*/mrbam/*")
-                mrbam_mv <- paste0(rawout,"/",tumor_df[i,1],"/MrBam*")
-                varscan_in <- paste0(rawout,"/",tumor_df[i,1],"/varscan*")
-                varscan_rn_1_in <- paste0(rawout,"/",tumor_df[i,1],"/0001.varscan-annovar-pair_indel_txt_output_18.txt")
-                varscan_rn_1_out <- paste0(rawout,"/",tumor_df[i,1],"/",tumor_df[i,1],"_indel_annovar.hg19_multianno.txt")
-                varscan_rn_2_in <- paste0(rawout,"/",tumor_df[i,1],"/0001.varscan-annovar-pair_snv_txt_output_18.txt")
-                varscan_rn_2_out <- paste0(rawout,"/",tumor_df[i,1],"/",tumor_df[i,1],"_snv_annovar.hg19_multianno.txt")
-                varscan_rename <- paste0('"s/sample/',tumor_df[i,1],'/"')
                 baseline <- paste0(tumor_df[i, 2], "_", tumor_df[i, 23])
-                fusion_json  <- paste0(rawout, "/", tumor_df[i, 1], "/fusionscan/", tumor_df[i, 1], "_fusion.json")
-                fusion_bam  <- paste0(rawout, "/", tumor_df[i, 1], "/", tumor_df[i, 1], "_rg.bam")
-                cnv_result <- paste0(rawout, "/", tumor_df[i, 1], "/cnv/", tumor_df[i, 1], "_rg_cnv_result.txt")
-                cnv_wesplus <- paste0(rawout, "/", tumor_df[i, 1], "/cnv/", tumor_df[i, 1], "_rg_cnv_genesWESPLUS.csv")
-                Annokb_snv_in <- paste0(rawout, "/", tumor_df[i, 1],"/", tumor_df[i, 2], "_", tumor_df[i, 23] ,".snv-nobias-GB18030-baseline.csv")
-                Annokb_snv_out <- paste0(rawout, "/", tumor_df[i, 1],"/Annokb_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23] ,".snv-nobias-GB18030-baseline-wesplus.csv")
-                Annokb_indel_in <- paste0(rawout, "/", tumor_df[i, 1],"/", tumor_df[i, 2], "_", tumor_df[i, 23] ,".indel-nobias-GB18030-baseline.csv")
-                Annokb_indel_out <- paste0(rawout, "/", tumor_df[i, 1],"/Annokb_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23] ,".indel-nobias-GB18030-baseline-wesplus.csv")
-                germline <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], ".germline.txt")
-                germline_out <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result")
-                cancer_female <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], ".cancer.female.txt")
-                cancer_female_trans <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.female.txt")
-                cancer_female_trans_xls <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.female.xls")
-                nocancer_female <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], ".nocancer.female.txt")
-                cancer_male <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], ".cancer.male.txt")
-                cancer_male_trans <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.male.txt")
-                nocancer_male <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], ".nocancer.male.txt")
+                fusion_json  <- paste0(out, "/fusionscan/", tumor_df[i, 1], "_fusion.json")
+                cnv_result <- paste0(out, "/cnv/", tumor_df[i, 1], "_rg_cnv_result.txt")
+                cnv_wesplus <- paste0(out, "/cnv/", tumor_df[i, 1], "_rg_cnv_genesWESPLUS.csv")
+                Annokb_snv_in <- paste0(out,"/", tumor_df[i, 2], "_", tumor_df[i, 23] ,".snv-nobias-GB18030-baseline.csv")
+                Annokb_snv_out <- paste0(out,"/Annokb_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23] ,".snv-nobias-GB18030-baseline-wesplus.csv")
+                Annokb_indel_in <- paste0(out,"/", tumor_df[i, 2], "_", tumor_df[i, 23] ,".indel-nobias-GB18030-baseline.csv")
+                Annokb_indel_out <- paste0(out,"/Annokb_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23] ,".indel-nobias-GB18030-baseline-wesplus.csv")
+                germline <- paste0(out, "/germline/result/", tumor_df[i, "normal"], ".germline.txt")
+                germline_out <- paste0(out, "/germline/result")
+                cancer_female <- paste0(out, "/germline/result/", tumor_df[i, "normal"], ".cancer.female.txt")
+                cancer_female_trans <- paste0(out, "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.female.txt")
+                cancer_female_trans_xls <- paste0(out, "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.female.xls")
+                nocancer_female <- paste0(out, "/germline/result/", tumor_df[i, "normal"], ".nocancer.female.txt")
+                cancer_male <- paste0(out, "/germline/result/", tumor_df[i, "normal"], ".cancer.male.txt")
+                cancer_male_trans <- paste0(out, "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.male.txt")
+                nocancer_male <- paste0(out, "/germline/result/", tumor_df[i, "normal"], ".nocancer.male.txt")
                 tsv_dir <- paste0(out,"/tsv")
                 mrbam_snv <- paste0(out, "/",baseline,".snv_MrBam.txt")
                 mrbam_indel <- paste0(out, "/",baseline,".indel_MrBam.txt")
                 tsv_snv <- paste0(tsv_dir,out,"/",baseline,".snv_MrBam.tsv")
                 tsv_indel <- paste0(tsv_dir,out,"/",baseline,".indel_MrBam.tsv")
-                cancer_male_trans_xls <- paste0(rawout, "/", tumor_df[i, 1], "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.male.xls")
-                Curl_snv_in <- paste0(rawout, "/", tumor_df[i, 1],"/Curl_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23], ".snv-nobias-GB18030-baseline-wesplus.csv")
-                Curl_snv_out <- paste0(rawout, "/", tumor_df[i, 1], "/NEW-Annokb-", tumor_df[i, 2], "_", tumor_df[i, 23], ".snv-wesplus.csv")
-                Curl_indel_in <- paste0(rawout, "/", tumor_df[i, 1],"/Curl_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23], ".indel-nobias-GB18030-baseline-wesplus.csv")
-                Curl_indel_out <- paste0(rawout, "/", tumor_df[i, 1], "/NEW-Annokb-", tumor_df[i, 2], "_", tumor_df[i, 23], ".indel-wesplus.csv")
+                cancer_male_trans_xls <- paste0(out, "/germline/result/", tumor_df[i, "normal"], "_trans.cancer.male.xls")
+                Curl_snv_in <- paste0(out,"/Curl_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23], ".snv-nobias-GB18030-baseline-wesplus.csv")
+                Curl_snv_out <- paste0(out, "/NEW-Annokb-", tumor_df[i, 2], "_", tumor_df[i, 23], ".snv-wesplus.csv")
+                Curl_indel_in <- paste0(out,"/Curl_mrbam_", tumor_df[i, 2], "_", tumor_df[i, 23], ".indel-nobias-GB18030-baseline-wesplus.csv")
+                Curl_indel_out <- paste0(out, "/NEW-Annokb-", tumor_df[i, 2], "_", tumor_df[i, 23], ".indel-wesplus.csv")
 cat(paste("###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
- tumor_df[i, 1], "/tmp/MrBam_pair_node_16/ ", out,"/MrBam_pair_node_16/\n",
+tumor_df[i, 1], "/tmp/MrBam_pair_node_16/mrbam/ ", out,"/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
- tumor_df[i, 1], "/tmp/MrBam_pair_node_17/ ", out,"/MrBam_pair_node_17/\n",
+ tumor_df[i, 1], "/tmp/MrBam_pair_node_17/mrbam/ ", out,"/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_", 
-tumor_df[i, 1], "/tmp/MutScan_node_24/ ",out, "/MutScan_node_24/\n",
+tumor_df[i, 1], "/tmp/MutScan_node_24/ ",out, "/mutscan_",tumor_df[i,1],"\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/cnv-pair_node_25/ ", out,"/cnv-pair_node_25/\n",
+tumor_df[i, 1], "/tmp/cnv-pair_node_25/ ", out,"/cnv/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/genefuse_huang_test_node_22/ ", out,"/genefuse_huang_test_node_22/\n",
+tumor_df[i, 1], "/tmp/genefuse_huang_test_node_22/ ", out,"/fusionscan/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/germline-sentieon-16core64g_node_38/result/ --include germline* ", out,"/germline-sentieon-16core64g_node_38/result/\n",
+tumor_df[i, 1], "/tmp/germline-sentieon-16core64g_node_38/result/ --include germline* ", out,"/hapyun/result/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/varscan-annovar-pair_node_18/ ", out,"/varscan-annovar-pair_node_18/\n",
+tumor_df[i, 1], "/tmp/varscan-annovar-pair_node_18/ ", out,"/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/fastp-16core64g_node_39/reports/ --include S* ", out,"/fastp-16core64g_node_39/reports/\n",
+tumor_df[i, 1], "/tmp/fastp-16core64g_node_39/reports/ --include S* ", out,"/QC/\n",
+"###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
+tumor_df[i, 1], "/tmp/fastp-16core64g_node_40/reports/ --include S* ", out,"/QC/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
 tumor_df[i, 1], "/tmp/Visual_msi_node_43/ ", out,"/msi/\n",
-"###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/fastp-16core64g_node_40/reports/ --include S* ", out,"/fastp-16core64g_node_40/reports/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
 tumor_df[i, 1], "/tmp/captureByRead_ave_depth_node_41/depth/ave_depth.stat ", out, "/Capture_Depth_cfdna/\n",
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
@@ -119,23 +90,23 @@ tumor_df[i, 1], "/tmp/captureByRead_ave_depth_node_42/capture/ --include *captur
 "###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
 tumor_df[i, 1], "/tmp/captureByRead_ave_depth_node_41/depth/ave_depth.stat ", out, "/Capture_Depth_gdna/\n",
 "###\n","echo ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/sentieon-out-sort-rmdup-bam_node_37/0001.sentieon-bwa-sort-gencore-out-sort-rmdup-bam_sortbam_cfdna.bam ./ >> /haplox/rawout/sort.txt\n",
+tumor_df[i, 1], "/tmp/sentieon-out-sort-rmdup-bam_node_37/0001.rgbam.bam ./ >> /haplox/rawout/sort.txt\n",
 "###\n","echo ossutil cp -r oss://sz-hapres/haplox/hapyun/201902/pair_sentieon_16core64g_no_factera_",
-tumor_df[i, 1], "/tmp/sentieon-out-sort-rmdup-bam_node_36/0001.sentieon-bwa-sort-gencore-out-sort-rmdup-bam_sortbam_gdna.bam ./ >> /haplox/rawout/sort.txt\n",
-"###\n","mv ", cnv_in, " ",  cnv_out, "\n",
-"###\n","mkdir ",qc_out, "\n","###\n","mv ",qc_in, " ",qc_out, "/\n","###\n","rm -r ", out, "/fastp*\n",
-"###\n","rename ", cnv_rename ," ", cnv_out, "/*\n", "###\n","mv ", MutScan_in , " ", MutScan_out , "\n", 
-"###\n","rename ", MutScan_rename ," ",  MutScan_out,"/*\n","###\n","mv ",fusionscan_in ," ", fusionscan_out,"\n", 
-"###\n","mv ",fusionscan_rn_1_in , " ", fusionscan_rn_1_out,"\n","###\n","mv ", fusionscan_rn_2_in ," ", fusionscan_rn_2_out,"\n",
-"###\n","mv ",germline_mv_1, " ", germline_mv_out, "\n", "###\n" ,"rename ",germline_rn, " ", germline_mv_out, "/result/*","\n",
-"###\n","mv ", germline_mv_out, " ", germline_mv_2, "\n", "###\n", "mv ", mrbam_in ," ", out, "/", "\n",
-"###\n","rm -r ",mrbam_mv,"\n", "###\n","mv  ",varscan_in ,"/*"," ",out,"\n","###\n","mv ",varscan_rn_1_in ," ",varscan_rn_1_out,"\n",
-"###\n","mv ", varscan_rn_2_in ," ", varscan_rn_2_out, "\n", "###\n" ,"rename ",varscan_rename ," ", out, "/*","\n", 
-"###\n","rm -r " ,varscan_in, "\n","###\n","mv ",out,"/msi/0001.Visual_msi_html_output_43.html ",out,"/msi/",csv_df[i,1],"_msi.html\n",
-"###\n","mv ",out,"/msi/0001.Visual_msi_json_output_43.json ",out,"/msi/",csv_df[i,1],"_msi.json\n",
-"###\n","mv ", out,"/Capture_Depth_cfdna/0001.sentieon-bwa-sort-gencore-out-sort-rmdup-bam_sortbam_cfdna_capture_stat.txt ",
+tumor_df[i, 1], "/tmp/sentieon-out-sort-rmdup-bam_node_36/0001.rgbam.bam ./ >> /haplox/rawout/sort.txt\n",
+"###\n","rename ", '"s/0001.sample/',tumor_df[i,1],'/"'," ", out, "/cnv/*\n",
+"###\n","rename ",'"s/0001.out/',tumor_df[i,1],'_mutscan/"' ," ", out,"/mutscan_",tumor_df[i,1],"/*\n",
+"###\n","mv ",out,"/fusionscan/0001.out_html.html ",out,"/fusionscan/",tumor_df[i,1],"_fusion.html \n", 
+"###\n","mv ",out,"/fusionscan/",tumor_df[i,1],"_fusion.json ",  out,"/fusionscan/",tumor_df[i,1],"_fusion.json \n",
+"###\n" ,"rename ",'"s/germline/',tumor_df[i,"normal"],'/"', " ", out, "/hapyun/result/*","\n",
+"###\n","mv ", out, "/hapyun/ " , out, "/germline/ \n",
+"###\n","mv  ",out,"/0001.indel_txt.txt ",out,"/",tumor_df[i,1],"_indel_annovar.hg19_multianno.txt\n",
+"###\n","mv  ",out,"/0001.snv_txt.txt ",out,"/",tumor_df[i,1],"_snv_annovar.hg19_multianno.txt\n",
+"###\n" ,"rename ",'"s/sample/',tumor_df[i,1],'/"'," ", out, "/*","\n", 
+"###\n","mv ",out,"/msi/0001.html.html ",out,"/msi/",csv_df[i,1],"_msi.html\n",
+"###\n","mv ",out,"/msi/0001.json.json ",out,"/msi/",csv_df[i,1],"_msi.json\n",
+"###\n","mv ", out,"/Capture_Depth_cfdna/0001.sortbam_capture_stat.txt ",
 out,"/Capture_Depth_cfdna/", tumor_df[i,1],"_sortbam_capture_stat.txt\n",
-"###\n","mv ", out,"/Capture_Depth_gdna/0001.sentieon-bwa-sort-gencore-out-sort-rmdup-bam_sortbam_gdna_capture_stat.txt ",
+"###\n","mv ", out,"/Capture_Depth_gdna/0001.sortbam_capture_stat.txt ",
 out,"/Capture_Depth_gdna/", tumor_df[i,"normal"],"_sortbam_capture_stat.txt\n",
 "###\n","mv ", out,"/Capture_Depth_cfdna/ave_depth.stat ",out,"/Capture_Depth_cfdna/",tumor_df[i,1],"_ave_depth.stat\n",
 "###\n","mv ", out,"/Capture_Depth_gdna/ave_depth.stat ",out,"/Capture_Depth_gdna/",tumor_df[i,"normal"],"_ave_depth.stat\n",
@@ -148,7 +119,7 @@ out,"/Capture_Depth_gdna/", tumor_df[i,"normal"],"_sortbam_capture_stat.txt\n",
 "###\n", "/haplox/thinker/net/ctDNA/samplemutationimport  -i ", tsv_indel, "\n",
 "###\n", "/haplox/thinker/net/ctDNA/mutationimport -mysql=192.168.1.14:3306 -i ", tsv_snv, "\n",
 "###\n", "/haplox/thinker/net/ctDNA/mutationimport -mysql=192.168.1.14:3306 -i ", tsv_indel, "\n", 
-"###\n", "Rscript /haplox/users/huang/mypy/data-analysis/ctdna_exome_pipeline/nobias_getMrBam_txt.R ", out, " ", baseline ,"\n",
+"###\n", "Rscript /haplox/users/huang/mypy/data-analysis/ctdna_exome_pipeline/nobias_getMrBam_txt_wesplus.R ", out, " ", baseline ,"\n",
 "###\n", "python /haplox/users/yangbo/futionbase.py -f ", fusion_json ,  "\n",
 "###\n", "Rscript /haplox/users/huang/mypy/data-analysis/ctdna_exome_pipeline/getCnv.R", " ",
           "/haplox/users/huang/mypy/data-analysis/ctdna_exome_pipeline/cnv/final.cnv.genelist ",cnv_result," ", cnv_wesplus,"\n",
@@ -167,7 +138,7 @@ out,"/Capture_Depth_gdna/", tumor_df[i,"normal"],"_sortbam_capture_stat.txt\n",
 "###\n","mkdir ",wk,"/",tumor_df[i,23],"\n","###\n","cp -r ",out,"/NEW*", " ", wk,"/",tumor_df[i,23], "/\n",
 "###\n","cp -r ",out, "/cnv/*cnv_genesWESPLUS.csv", " ", wk,"/",tumor_df[i,23],"/\n",
 "###\n","cp -r ",out,"/germline/result/*trans.cancer*curl.xls"," ",wk,"/",tumor_df[i,23],"/\n",
-"###\n","python3 /haplox/users/zhaoys/Hap_HPC/pair_ttdna_wes_warning.py -d ",rawout,"/",tumor_df[i,1],"\n",sep = ""))
+"###\n","python3 /haplox/users/zhaoys/Hap_HPC/pair_ttdna_wes_warning.py -d ",out,"\n",sep = ""))
                  sink()
             }
         }
