e20d631d1612de8117736faaa368ae53e59313b7
zys <1255886933@qq.com>
2019-03-07 17:10:31 +0800

    update

--- dezip_csv.R
+++ dezip_csv.R
@@ -8,10 +8,10 @@ csv_df5 <- NULL
 sampleSheet <- paste0("/haplox/runPipelineInfo/",  input,  "/sequence_", input,  ".csv")
 sampleSheet2 <- paste0("/haplox/runPipelineInfo/",  input2,  "/sequence_", input2,  ".csv")
 
-    csv_df1 <- read.csv(sampleSheet, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="latin1", encoding="UTF-8")
-    csv_df2 <- read.csv(sampleSheet2, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="latin1", encoding="UTF-8")
-    csv_df3 <- csv_df1[csv_df1[, 9] %in% c("cfdna", "healthcfdna", "ttdna", "ffpedna"), ]
-#    print(csv_df3[,1])
+    csv_df1 <- read.csv(sampleSheet, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="GBK")
+    csv_df2 <- read.csv(sampleSheet2, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="GBK")
+    csv_df3 <- csv_df1[csv_df1[, 9] %in% c("cfdna", "healthcfdna", "ttdna", "ffpedna") & grepl("肠康",csv_df1[,25]), ]
+#    print(csv_df3[,15])
     if(nrow(csv_df3) <= 0){
         print("no data for tumor")
         quit(save = "no")
@@ -39,22 +39,29 @@ sampleSheet2 <- paste0("/haplox/runPipelineInfo/",  input2,  "/sequence_", input
     myStrsplit <- function(x, split_para){
         unlist(strsplit(x, split=split_para))[8]
     }
+    date <- Sys.Date()
     for(i in seq(nrow(csv_df3))){
             for(j in seq(nrow(csv_df4))){
-                if(csv_df3[i, 3] == csv_df4[j, 3]){
+                if(csv_df3[i, 3] == csv_df4[j, 3] && csv_df3[i, 15] == csv_df4[j, 15]){
                     restore_dir <- txt_input[grepl(csv_df4[j,1], txt_input)] 
                     print(restore_dir)
-                    oss_restore <- paste0("/haplox/users/zhaoys/dezip/oss_restore.sh")
+                    oss_restore <- paste0("/haplox/users/zhaoys/dezip/oss_restore_",gsub("-","",date),".sh")
+                    oss_download <- paste0("/haplox/users/zhaoys/dezip/oss_download_",gsub("-","",date),".sh")
                     sink(oss_restore,append =TRUE)
                     cat(paste0("###\n","ossutil restore ",restore_dir,"\n",step=""))
                     sink()
                     dir <- gsub("oss://sz-hapseq","",restore_dir)
                     csv_df5 <- c(csv_df5,dir)
                     print(csv_df5)
+                    sink(oss_download,append = TRUE)
+                    cat(paste0("###\n","ossutil cp -r oss://sz-hapres/haplox/hapyun/",substr(gsub("-","",date),1,6),
+                    "/tmp_workflow_repaq-xz-rawfq_1_",gsub("-","_",date),"/tmp/repaq-xz-rawfq_node_2/ --include ",
+                    csv_df4[j,1],"* oss://sz-hapseq/rawfq/","20", substr(input2, 1, 4), "/", input2,  "_clinic/","\n",step=""))
+                    sink()
                 }
             }
         }
-out_csv <- paste0("/haplox/users/zhaoys/dezip/dezip.csv")
+out_csv <- paste0("/haplox/users/zhaoys/dezip/dezip_",gsub("-","",date),".csv")
 if(!file.exists(out_csv)){
     file.create(out_csv, recursive = TRUE)
     }else{
@@ -66,3 +73,4 @@ for(i in seq(length(csv_df5))){
     cat(paste0(csv_df5[i],",", csv_df5[i],"\n", step=""))
 }
 sink()
+
