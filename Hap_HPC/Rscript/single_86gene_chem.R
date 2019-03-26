df42878039ec48ffa149d62cb199bd1117161de8
zys <1255886933@qq.com>
2019-03-26 15:08:32 +0800

    single 86gene chem

--- Hap_HPC/Rscript/single_86gene_chem.R
+++ Hap_HPC/Rscript/single_86gene_chem.R
@@ -1,13 +1,12 @@
 #!/usr/bin/Rscript
-print("move attached sheet")
 
 args <- commandArgs(TRUE)
 path <- args[1]
 input <- strsplit(path,"/")[[1]][4]
-print(input)
+#print(input)
 sample <- stringi::stri_reverse(strsplit(stringi::stri_reverse(path),"/")[[1]][2])
-print(sample)
-sheet_dir <- paste0("/x01_haplox/users/zhaoys/Script/")
+#print(sample)
+sheet_dir <- paste0("/x01_haplox/hapreports/3.遗传咨询待发送/00.待审核x小panel/")
 sampleSheet <- paste0("/haplox/runPipelineInfo/",  input,  "/sequence_", input,  ".csv")
 csv_df <- read.csv(sampleSheet, header=FALSE, stringsAsFactors = FALSE,  fileEncoding="GBK")
 #print(csv_df[,15])
@@ -20,10 +19,14 @@ if(nrow(tumor_df)>=1){
             if(grepl("11|90",tumor_df[i,24])){
                 system(paste0("cp ",path,"germline/result/",sample,".chem_90.xls ",sheet_dir,"\n",
                 "mv ",sheet_dir,sample,".chem_90.xls ",sheet_dir,tumor_df[i,22],"-",tumor_df[i,3],"-",tumor_df[i,7],".chem_90.xls ","\n"))
-                }
-                if(grepl("51",tumor_df[i,24])){
-                    system(paste0("mv ",path,"germline/result/",sample,".chem_51.xls ",sheet_dir,"\n",
-                    "mv ",sheet_dir,sample,".chem_51.xls ",sheet_dir,tumor_df[i,22],"-",tumor_df[i,3],"-",tumor_df[i,7],".chem_51.xls ","\n"))
+            }
+            if(grepl("51",tumor_df[i,24])){
+                system(paste0("mv ",path,"germline/result/",sample,".chem_51.xls ",sheet_dir,"\n",
+                "mv ",sheet_dir,sample,".chem_51.xls ",sheet_dir,tumor_df[i,22],"-",tumor_df[i,3],"-",tumor_df[i,7],".chem_51.xls ","\n"))
+            }
+            if(grepl("37",tumor_df[i,24])){
+                system(paste0("mv ",path,"germline/result/",sample,".chem_37.xls ",sheet_dir,"\n",
+                "mv ",sheet_dir,sample,".chem_51.xls ",sheet_dir,tumor_df[i,22],"-",tumor_df[i,3],"-",tumor_df[i,7],".chem_37.xls ","\n"))
             }
        }
    }
